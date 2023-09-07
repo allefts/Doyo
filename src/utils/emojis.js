8a59ccc07785bd1e058e153ffd119c7137ca112d
allefts <alleftsoares@gmail.com>
2023-09-06 23:41:38 -0500

    tempToEmoji

---
+++ src/utils/emojis.js
@@ -0,0 +1,48 @@
+const emojis = {
+  cloud: "☁️",
+  sun: "☀️",
+  night: "🌕",
+  rainCloudThunder: "⛈️",
+  rainCloud: "🌧️",
+  sunCloud: "🌤️",
+  snow: "❄️",
+  earth: "🌎",
+};
+
+const isDay = () => {
+  let currTime = new Date().getHours();
+  let day = true;
+  currTime > 12 ? (day = false) : (day = true);
+
+  return day;
+};
+
+export const tempToEmoji = (main) => {
+  //Receives a temp main and returns corresponding emoji/icon
+  let emoji = "";
+  switch (main) {
+    case "Clear":
+      emoji = isDay() ? emojis.sun : emojis.night;
+      break;
+    case "Clouds":
+      emoji = emojis.cloud;
+      break;
+    case "Thundestorm":
+      emoji = emojis.rainCloudThunder;
+      break;
+    case "Rain":
+      emoji = emojis.rainCloud;
+      break;
+    case "Drizzle":
+      emoji = emojis.rainCloud;
+      break;
+    case "Snow":
+      emoji = emojis.snow;
+      break;
+    default:
+      emoji = emojis.earth;
+      break;
+  }
+
+  return emoji;
+};
