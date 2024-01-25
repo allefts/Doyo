2d2e46719e523b51de10df1ef80b7cfea7ebcc16
allefts <alleftsoares@gmail.com>
2024-01-25 13:00:04 -0600

    need to find a free crypto exchange api

--- src/widgets/Crypto.js
+++ src/widgets/Crypto.js
@@ -5,4 +5,48 @@ export class CryptoWidget extends HTMLElement {
     super();
     this.append(cryptoTemplate.content.cloneNode(true));
   }
+
+  async connectedCallback() {
+    this.renderDefault();
+    const data = await this.getCrypto();
+  }
+
+  async getCrypto() {
+    const KEY = "";
+    const ENDPOINT = "";
+
+    // const res = fetch(ENDPOINT, {});
+    // console.log(res);
+    return;
+  }
+
+  renderDefault() {
+    this.innerHTML = `
+    <style>
+      h1,
+      h2,
+      h3 {
+        padding: 0;
+        margin: 0;
+        color: white;
+      }
+
+      #crypto-content {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+      }
+
+      .bitcoin-emoji {
+        color: yellow;
+      }
+    </style>
+     
+    <div id="crypto-content">
+      <h1 class="bitcoin-emoji">₿</h1>
+    </div>
+    `;
+  }
 }
